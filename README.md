6058a2c7736b19baa111527ce37c2638ff6633fb
AntonioGarnier <alu0100454437@ull.edu.es>
2017-03-12 16:52:45 +0000

    README terminado

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Práctica 2: Primeros pasos en NodeJS
 
-## Por [Antonio Jesús López Garnier](https://antoniogarnier.github.io/)
+### Por [Antonio Jesús López Garnier](https://antoniogarnier.github.io/)
+
+## Tareas:
+
+   1. Reescribir la estructura de directorios de nuestro anterior gitbook.
+   2. Convertir los scripts shell a javascript para poder crear y publicar un gitbook en la gh-pages.
+   3. Crear un gulpfile con las tareas "build", "deploy" y "serve" para automatizar la construcción del libro.
+   4. Desplegar el gitbook en el IAAS.
--- scripts/contributors.js
+++ scripts/contributors.js
@@ -9,7 +9,8 @@ require('simple-git')()
                if (!contribuidores.includes(log.all[i].author_name))
                   contribuidores.push(log.all[i].author_name);
             for (var i = 0; i < contribuidores.length; i++)
-               console.log(contribuidores[i] + "\n");
+               console.log(contribuidores[i]);
+            console.log("Último commit realizado: " + log.all[log.all.length - 1].author_name + " - " + log.all[log.all.length - 1].date + " - " + log.all[log.all.length - 1].message);
             console.log("Último commit realizado: " + log.latest.author_name + " - " + log.latest.date + " - " + log.latest.message);
           }
         });
